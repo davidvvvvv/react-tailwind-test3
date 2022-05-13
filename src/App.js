8260f5fb81d0617be1348ef13701dab7a3602695
davidvvvvv <davidvvvvv@gmail.com>
2022-05-13 12:31:46 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <div className="bg-red-500">Learn React</div>
         </a>
       </header>
     </div>
